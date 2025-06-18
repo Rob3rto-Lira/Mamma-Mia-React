7122af8fee1a8ac2fb6bfd404a2bc8dcef43bae8
Rob3rto-Lira <rliraleon@gmail.com>
2025-06-18 17:12:52 -0400

    Made site's basic template

---
+++ mamma-mia/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./assets/CSS/style.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import CardPizza from "./components/CardPizza/CardPizza";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="layout-grid">
+        <nav className="navbar">
+          <Navbar />
+        </nav>
+        <div className="home">
+          <Home />
+          <CardPizza />
+          <CardPizza />
+          <CardPizza />
+        </div>
+        <footer className="footer">
+          <Footer />
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
