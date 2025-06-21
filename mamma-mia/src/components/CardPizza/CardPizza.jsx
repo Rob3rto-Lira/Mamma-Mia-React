1b9eecde3b782b5723cffc0b0c28fb4f3d0c8f5e
Rob3rto-Lira <rliraleon@gmail.com>
2025-06-21 15:53:16 -0400

    filled in cards

--- mamma-mia/src/components/CardPizza/CardPizza.jsx
+++ mamma-mia/src/components/CardPizza/CardPizza.jsx
@@ -1,20 +1,26 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+const formatTotal = (value) => {
+  const num = Number(value);
+  return isNaN(num) ? value : num.toLocaleString("es-CL");
+};
 const CardPizza = ({ img, title, ingredients, price }) => {
   return (
     <Card className="CardPizza">
       <Card.Img variant="top" src={img} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
+        <hr />
         <div style={{ textAlign: "center" }}>
           <p style={{ color: "gray" }}>Ingredientes:</p>
           <p>{ingredients}</p>
-          <h4>{price}</h4>
+          <hr />
+          <h4>${formatTotal(price)}</h4>
         </div>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Button variant="outline-dark">Go somewhere</Button>
-          <Button variant="dark">Go somewhere else</Button>
+          <Button variant="outline-dark">Ver más 👀</Button>
+          <Button variant="dark">Añadir 🛒</Button>
         </div>
       </Card.Body>
     </Card>
--- mamma-mia/src/components/Home/Home.jsx
+++ mamma-mia/src/components/Home/Home.jsx
@@ -12,25 +12,25 @@ const Home = () => {
           img={
             "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
           }
-          title="Some pizza"
-          ingredients="Tasty stuff"
-          price="What a bargain!"
+          title="Pizza Napolitana"
+          ingredients="🍕 mozarella, tomates, jamón, orégano"
+          price={5950}
         />
         <CardPizza
           img={
-            "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
+            "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"
           }
-          title="Some pizza"
-          ingredients="Tasty stuff"
-          price="What a bargain!"
+          title="Pizza Española"
+          ingredients="🍕 mozarella, gorgonzola, parmesano, provolone"
+          price={6950}
         />
         <CardPizza
           img={
-            "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
+            "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
           }
-          title="Some pizza"
-          ingredients="Tasty stuff"
-          price="What a bargain!"
+          title="Pizza Pepperoni"
+          ingredients="🍕 mozarella, pepperoni, orégano"
+          price={6950}
         />
       </div>
     </>
