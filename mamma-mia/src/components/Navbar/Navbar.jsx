3cdfaada3ad99969258a72964dc48b3508dd0b95
Rob3rto-Lira <rliraleon@gmail.com>
2025-06-21 15:16:30 -0400

    Navbar

--- mamma-mia/src/components/Navbar/Navbar.jsx
+++ mamma-mia/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,26 @@
 import Button from "react-bootstrap/Button";
 
 const Navbar = () => {
+  const total = 25000;
+  const token = false;
+  const formatTotal = (num) => num.toLocaleString("es-CL");
+
   return (
     <>
       <Button variant="danger">🍕 Home</Button>
-      <Button variant="danger">🔓 Profile</Button>
-      <Button variant="danger">🔒 Logout</Button>
-      <Button variant="danger">🔐 Login</Button>
-      <Button variant="danger">🔐 Register</Button>
-      <Button variant="danger">🛒 Total: $</Button>
+      {token ? (
+        <>
+          <Button variant="danger">🔓 Profile</Button>
+          <Button variant="danger">🔒 Logout</Button>
+        </>
+      ) : (
+        <>
+          <Button variant="danger">🔐 Login</Button>
+          <Button variant="danger">🔐 Register</Button>
+        </>
+      )}
+
+      <Button variant="danger">🛒 Total: ${formatTotal(total)}</Button>
     </>
   );
 };
